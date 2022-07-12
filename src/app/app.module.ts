58a2a014d1573459cda3fd39c9cad34ebebacccd
patshone96 <patrick_oliver_@hotmail.es>
2022-07-12 12:20:20 +0200

    First Update on Angular Tutorial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { empleadoscomponent } from './Empleados/empleados.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, empleadoscomponent, EmpleadoComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+
+  template: "<p>Aquí iria un empleado</p>",
+  templateUrl: './empleado.component.html',
+  
+  styleUrls:['./empleado.component.css']
+
+  //styles:["p{background-color:red;}"]
+})
+export class EmpleadoComponent implements OnInit {
+
+  nombre = "Juan";
+
+  apellido = "Díaz";
+
+  private edad = 7;
+
+  //empresa = "Píldoras Informáticas";
+
+  llamaEmpresa(value:String){
+    
+
+
+
+
+  }
+
+  getEdad(){
+    return this.edad
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
